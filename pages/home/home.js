6049a26b71c507e1dbbf8fc100b1ae7fa9a1ebae
mrlzl <1254348304@qq.com>
2020-03-15 11:26:23 +0800

    事件类型练习

--- pages/home/home.js
+++ pages/home/home.js
@@ -11,6 +11,8 @@ Component({
    * 组件的初始数据
    */
   data: {
+    lists: ["小米", "小夜", "小岚"],
+    currentActive: 0
 
   },
 
@@ -18,6 +20,54 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    handlerTap1() {
+      console.log("handlerTap1")
+    },
+    captureBindHandlerTap1() {
+      console.log("captureBindHandlerTap1")
+    },
+    handlerTap2() {
+      console.log("handlerTap2")
+    },
+    captureBindHandlerTap2() {
+      console.log("captureBindHandlerTap2")
+    },
+    handlerTap3() {
+      console.log("handlerTap3")
+    },
+    captureBindHandlerTap3() {
+      console.log("captureBindHandlerTap3")
+    },
 
+
+    // --------------事件对象函数测试
+    start(e) {
+      console.log("start", e)
+    },
+    end(e) {
+      console.log("end", e)
+    },
+    move(e) {
+      console.log("move", e)
+    },
+    cancel(e) {
+      console.log("cancel".e)
+    },
+    tap(e) {
+      console.log("tap", e)
+    },
+    press(e) {
+      console.log("press", e)
+    },
+
+    // ---------------Exercise 取出对象某个属性
+    clickName(e) {
+      const property = e.currentTarget.dataset;
+      let item = property.item;
+      let index = property.index;
+      this.setData({
+        currentActive: index
+      })
+    }
   }
 })
