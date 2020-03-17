5207dd0ef6869107d7f8fc80e5aad2f115b008a2
mrlzl <1254348304@qq.com>
2020-03-17 21:01:23 +0800

    网络疯转

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,23 +1,90 @@
-// pages/home/home.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
+import networkPromise from '../../server/network.js'
+Page({
 
   /**
-   * 组件的初始数据
+   * 页面的初始数据
    */
   data: {
 
   },
 
   /**
-   * 组件的方法列表
+   * 生命周期函数--监听页面加载
    */
-  methods: {
+  onLoad: function(options) {
+    // 原生的网络请求
+    // this.originNetworkRequest()
+
+    //封装一个Promise网络请求
+    networkPromise({
+      url: 'http://123.207.32.32:8000/api/wh/recommend'
+    }).then(res=>{
+      console.log(res)
+    }).catch(err=>{
+      console.log(err)
+    })
+    
+  },
+  originNetworkRequest(){
+    wx.request({
+      url: 'http://httpbin.org/post',
+      method: "post",
+      data: {
+        name: "lzw",
+        age: 17
+      },
+      success: (res) => {
+        console.log(res)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
 
   }
 })
